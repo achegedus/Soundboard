a71028f7e39ed94e3a48ec347ed92c99624fc85a
Adam Hegedus <adam.hegedus@energycap.com>
2015-03-24 17:11:15 -0400

    new sound view controller

---
+++ SoundboardApp/Sound.swift
@@ -0,0 +1,16 @@
+//
+//  Sound.swift
+//  SoundboardApp
+//
+//  Created by Adam Hegedus on 3/24/15.
+//  Copyright (c) 2015 Adam Hegedus. All rights reserved.
+//
+
+import Foundation
+
+class Sound {
+    
+    var name = "Default"
+    var URL = NSURL()
+    
+}
--- SoundboardApp/SoundListViewController.swift
+++ SoundboardApp/SoundListViewController.swift
@@ -15,23 +15,42 @@ class SoundListViewController: UIViewController, UITableViewDataSource, UITableV
     
     var audioPlayer = AVAudioPlayer()
     
+    var sounds: [Sound] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         self.tableView.dataSource = self
         self.tableView.delegate = self
+        
+        var soundPath = NSBundle.mainBundle().pathForResource("TestSound", ofType: "m4a")
+        var soundURL = NSURL.fileURLWithPath(soundPath!)
+        
+        var sound1 = Sound()
+        sound1.name = "Blah Blah"
+        sound1.URL = soundURL!
+        
+        self.sounds.append(sound1)
+        
+        
+        var sound2 = Sound()
+        sound2.name = "Turn down for what?"
+        sound2.URL = soundURL!
+        
+        self.sounds.append(sound2)
     }
     
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return self.sounds.count
     }
     
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         var cell = UITableViewCell()
-        cell.textLabel!.text = "Hello"
+        var sound = self.sounds[indexPath.row]
+        cell.textLabel!.text = sound.name
         return cell
     }
     
