3e5afbfccc5aa0d0e6c83ab9dfc03801f993b85a
Adam Hegedus <adam.hegedus@energycap.com>
2015-03-27 17:03:43 -0400

    Finished app

--- SoundboardApp/NewSoundViewController.swift
+++ SoundboardApp/NewSoundViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import AVFoundation
+import CoreData
 
 class NewSoundViewController : UIViewController {
     
@@ -15,9 +16,11 @@ class NewSoundViewController : UIViewController {
     required init(coder aDecoder: NSCoder) {
         var baseString : String = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)[0] as String
         
-        var pathComponents = [baseString, "MyAudio.m4a"]
+        self.audioURL = NSUUID().UUIDString + ".m4a"
         
-        self.audioURL = NSURL.fileURLWithPathComponents(pathComponents)!
+        var pathComponents = [baseString, self.audioURL]
+        
+        var audioNSURL = NSURL.fileURLWithPathComponents(pathComponents)!
         
         var session = AVAudioSession.sharedInstance()
         session.setCategory(AVAudioSessionCategoryPlayAndRecord, error: nil)
@@ -27,7 +30,7 @@ class NewSoundViewController : UIViewController {
         recordSettings[AVSampleRateKey] = 44100.0
         recordSettings[AVNumberOfChannelsKey] = 2
         
-        self.audioRecorder = AVAudioRecorder(URL: audioURL, settings: recordSettings, error: nil)
+        self.audioRecorder = AVAudioRecorder(URL: audioNSURL, settings: recordSettings, error: nil)
         self.audioRecorder.meteringEnabled = true
         self.audioRecorder.prepareToRecord()
         
@@ -39,7 +42,7 @@ class NewSoundViewController : UIViewController {
     @IBOutlet weak var recordButton: UIButton!
     
     var audioRecorder : AVAudioRecorder
-    var audioURL : NSURL
+    var audioURL : String
     var previousSoundListViewController = SoundListViewController()
     
     override func viewDidLoad() {
@@ -55,13 +58,16 @@ class NewSoundViewController : UIViewController {
     
     
     @IBAction func saveTapped(sender: AnyObject) {
-        // create a sound object
-        var sound = Sound()
-        sound.name = self.soundName.text
-//        sound.URL = self.audioURL
         
-        // add sound to sound array
-        self.previousSoundListViewController.sounds.append(sound)
+        var context = (UIApplication.sharedApplication().delegate as AppDelegate).managedObjectContext!
+        
+        // create a sound object
+        var sound = NSEntityDescription.insertNewObjectForEntityForName("Sound", inManagedObjectContext: context) as Sound
+        sound.name = self.soundName.text
+        sound.url = self.audioURL
+        
+        // save sound to core data
+        context.save(nil)
         
         // dismiss view controller
         self.dismissViewControllerAnimated(true, completion: nil)
--- SoundboardApp/SoundListViewController.swift
+++ SoundboardApp/SoundListViewController.swift
@@ -24,30 +24,17 @@ class SoundListViewController: UIViewController, UITableViewDataSource, UITableV
         
         self.tableView.dataSource = self
         self.tableView.delegate = self
+    }
+    
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
         
-        var soundPath = NSBundle.mainBundle().pathForResource("TestSound", ofType: "m4a")
-        var soundURL = NSURL.fileURLWithPath(soundPath!)
-        
+        // GO
         var context = (UIApplication.sharedApplication().delegate as AppDelegate).managedObjectContext!
-        var sound1 = NSEntityDescription.insertNewObjectForEntityForName("Sound", inManagedObjectContext: context) as Sound
-        sound1.name = "My Sound"
-        sound1.url = soundURL!.absoluteString!
-        
-        context.save(nil)
-        
         var request = NSFetchRequest(entityName: "Sound")
-        
+   
         self.sounds = context.executeFetchRequest(request, error: nil)! as [Sound]
-        
-//        var sound1 = Sound()
-//        sound1.name = "Blah Blah"
-//        sound1.URL = soundURL!
-//        self.sounds.append(sound1)
-//        
-//        var sound2 = Sound()
-//        sound2.name = "Turn down for what?"
-//        sound2.URL = soundURL!
-//        self.sounds.append(sound2)
     }
     
     
@@ -71,8 +58,13 @@ class SoundListViewController: UIViewController, UITableViewDataSource, UITableV
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         var sound = self.sounds[indexPath.row]
+        var baseString : String = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)[0] as String
+        var pathComponents = [baseString, sound.url]
+        var audioNSURL = NSURL.fileURLWithPathComponents(pathComponents)!
         
-//        self.audioPlayer = AVAudioPlayer(contentsOfURL: sound.URL, error: nil)
+        self.tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        
+        self.audioPlayer = AVAudioPlayer(contentsOfURL: audioNSURL, error: nil)
         self.audioPlayer.play()
     }
 
